6ce8adeb0c88c2ed7c63ce1ce19e286728c89aa7
tmkk2202 <22thetmon@gmail.com>
2024-11-05 20:24:07 +0700

    stopwatch practice done!

--- stopWatch/script.js
+++ stopWatch/script.js
@@ -1,63 +1,58 @@
-const clockDiv = document.querySelector(".clockDiv")
-const start = document.querySelector(".start");
-const pause = document.querySelector(".pause");
-const conti = document.querySelector(".continue");
-const reset = document.querySelector(".reset");
-const msecDiv = document.querySelector('.msecond');
 
-let second = 0, minute = 0, hour = 0, msec =0 ;
+    
+const clockDiv = document.querySelector('.clockDiv');
+const msecond = document.querySelector('.msecond');
+const start = document.querySelector('.start');
+const pause = document.querySelector('.pause');
+const conti = document.querySelector('.continue');
+const reset = document.querySelector('.reset');
 
-let startfunc = () => {
-    msec += 1;
-    if (msec >= 1000) {
-        msec = 0;
+let hour = 0, minute = 0, second = 0, milisecond = 0;
+let x;
+
+const displayFunc = () => {
+    const msText = milisecond < 10 ? "00" + milisecond.toString() : milisecond < 100 ? "0" + milisecond.toString() : milisecond;
+    const secText = second < 10 ? "0" + second.toString() : second;
+    const minText = minute < 10 ? "0" + minute.toString() : minute;
+    const hrText = hour < 10 ? "0" + hour.toString() : hour;
+    clockDiv.textContent = `${ hrText } : ${ minText } : ${ secText }`;
+    msecond.textContent = msText;
+}
+const func = () => {
+    milisecond += 1;
+    if (milisecond === 1000) {
         second += 1;
+        milisecond = 0;
         if (second === 60) {
-            second = 0;
             minute += 1;
+            second = 0;
             if (minute === 60) {
-                minute = 0;
                 hour += 1;
+                minute = 0;
             }
         }
     }
-    displayFunc();    
-}
-
-let displayFunc = () => {
-    const secText = second < 10 ? "0" + second.toString() : second;
-    const minText = minute < 10 ? "0" + minute.toString() : minute;
-    const hrText = hour < 10 ? '0'+ hour.toString() : hour;
-    const msecText = msec<10 ? "000" + msec.toString() : msec<100 ? "00" + msec.toString() : msec<1000 ? "0" + msec.toString() : msec;
-            
-    clockDiv.textContent = hrText + ":" + minText + ":" + secText;
-    msecDiv.textContent = msecText;
-
-}
-
-let yy;
-start.addEventListener('click', () => {
-    yy = setInterval(startfunc, 1);
     
+    displayFunc();
+        
+};
+
+
+start.addEventListener('click', () => {
+    x = setInterval(func, 1);
 });
 
 pause.addEventListener('click', () => {
-    clearInterval(yy);
-    yy = null;
-    
+    clearInterval(x);
 });
 
 conti.addEventListener('click', () => {
-    clearInterval(yy);   
-    yy = setInterval(startfunc, 1);
-    
+    clearInterval(x);
+    x = setInterval(func, 1);
 });
 
 reset.addEventListener('click', () => {
-    clearInterval(yy);
-    yy = null;
-    hour = 0, minute = 0, second = 0, msec = 0;
+    clearInterval(x);
+    hour = 0, minute = 0, second = 0, milisecond = 0;
     displayFunc();
-    
-    
 });
